e096722592fc548271610d02afbd23a97cb15937
永田大祐 <dbank0208@gmail.com>
2017-03-28 00:28:57 +0900

    self.tableViewSetting.reloadData()の対応

--- RealmSample/SearchBar/SearchBar.swift
+++ RealmSample/SearchBar/SearchBar.swift
@@ -14,20 +14,16 @@ extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         if  searchBar.text !=  "" {
-
+            
             //indexの値を渡す
             usersSet = realmTry.objects(realmDataSet.self)
                 .filter("ID BEGINSWITH %@", searchSet.text!)
                 .sorted(byProperty: "Message", ascending: false)
-            
-            self.tableViewSetting.reloadData()
-            
         }else{
             
             usersSet = realmTry.objects(realmDataSet.self)
                 .sorted(byProperty: "now", ascending: false)
-            
-            self.tableViewSetting.reloadData()
         }
+        self.tableViewSetting.reloadData()
     }
 }
