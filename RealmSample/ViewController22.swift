d24937368bb3431e7b8b1cee546992c255001c69
nagatadaisuke <dbank0208@gmail.com>
2017-07-16 13:51:14 +0900

    横幅の設定

--- RealmSample/ViewController22.swift
+++ RealmSample/ViewController22.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController22: UIViewController {
-
+    
     static var ddd = ViewController22()
     var now = NSDate()
     var tableViewSetting = ViewController().tableViewSetting
@@ -25,11 +25,34 @@ class ViewController22: UIViewController {
         tableViewSetting.frame = CGRect(x:0,y:64,width:UIScreen.main.bounds.width,height:UIScreen.main.bounds.height)
         view.addSubview(tableViewSetting)
         tableViewSetting.reloadData()
-
+        
     }
-
+    
     @IBAction func update(_ sender: UIBarButtonItem) {
         
         tableViewSetting.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        NotificationCenter.default.addObserver(self, selector: #selector(onOrientationChange(notification:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    func onOrientationChange(notification: NSNotification){
+        
+        if UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone {
+            let deviceOrientation: UIDeviceOrientation!  = UIDevice.current.orientation
+            
+            if UIDeviceOrientationIsLandscape(deviceOrientation) {
+                
+                tableViewSetting.frame = CGRect(x:-50,y:0,width:UIScreen.main.bounds.height*2,height:UIScreen.main.bounds.width)
+                
+            } else if UIDeviceOrientationIsPortrait(deviceOrientation){
+                
+                
+                tableViewSetting.frame = CGRect(x:0,y:64,width:UIScreen.main.bounds.width,height:UIScreen.main.bounds.height)
+                
+            }
+        }
     }
 }
