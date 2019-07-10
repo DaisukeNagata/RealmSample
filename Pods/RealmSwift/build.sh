ed18f489bcbdb1ace351d8df315efee9ebad7733
永田大祐 <dbank0208@gmail.com>
2019-07-10 23:50:54 +0900

    clear pods and workspace

--- Pods/RealmSwift/build.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a4949a9bc22f56b7538d112805fa53ffa138499fdddfdcbc0acfbf8a98a273c3
-size 51162
--- Pods/Target Support Files/Pods-RealmSample/Pods-RealmSample-frameworks.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:341e4015ee0c233ea7c0b2f1042ea661f7bf44a79a7320d2149a73a58eb6869b
-size 7647
